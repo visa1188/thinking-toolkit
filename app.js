db910f05438d4019014849ed02c907210fb766bf
Vidyasagar <vidyasagar.malempati@gmail.com>
2026-02-21 16:59:45 +0530

    added session compleiton buttons

--- app.js
+++ app.js
@@ -15,4 +15,24 @@ document.addEventListener("DOMContentLoaded", function () {
       obj.style.borderRadius = '10px';
     });
   });
+
+  const completeBtn = document.querySelector('.complete-btn');
+
+  if (completeBtn) {
+    const sessionKey = completeBtn.dataset.session;
+
+    // Check localStorage
+    if (localStorage.getItem(sessionKey) === "done") {
+      completeBtn.classList.add("completed");
+      completeBtn.textContent = "✓ Session Completed";
+      completeBtn.disabled = true;
+    }
+
+    completeBtn.addEventListener('click', () => {
+      localStorage.setItem(sessionKey, "done");
+      completeBtn.classList.add("completed");
+      completeBtn.textContent = "✓ Session Completed";
+      completeBtn.disabled = true;
+    });
+  }
 });
