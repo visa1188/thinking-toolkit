2874afa6e87752bbc166aa865ce697e8956a4fcb
Vidyasagar <vidyasagar.malempati@gmail.com>
2026-02-21 16:48:29 +0530

    Mobile Friendly Modifications

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   objects.forEach(obj => {
     obj.addEventListener('click', () => {
+
+      // Remove highlight from all
+      objects.forEach(o => {
+        o.style.outline = '';
+        o.style.borderRadius = '';
+      });
+
+      // Highlight selected
       obj.style.outline = '4px solid #4CAF50';
       obj.style.borderRadius = '10px';
     });
