df3a9c1bf1b65d50589f0ccd5bc30b9a32f43729
Vidyasagar <vidyasagar.malempati@gmail.com>
2026-02-22 07:25:46 +0530

    added about page

--- nav.js
+++ nav.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const navHTML = `
     <div class="nav">
       <a href="index.html">Home</a> |
-      <a href="sessions.html">Sessions</a>
-       <a href="about.html">About</a>
+      <a href="sessions.html">Sessions</a> |
+      <a href="about.html">About</a>
     </div>
   `;
 
